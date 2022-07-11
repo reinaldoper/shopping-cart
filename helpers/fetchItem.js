35a3db1900cff9df11171a1c4e5f8b467f0188ba
Reinaldo <reinaldoper83@gmail.com>
2022-07-11 19:31:21 -0300

    requisito 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,4 +1,12 @@
-const fetchItem = () => {
+const fetchItem = async (endPoint) => {
+  try {
+    const url = `https://api.mercadolibre.com/items/${endPoint}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    return error;
+  }
   // seu código aqui
 };
 
--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -3,6 +3,22 @@ const { fetchItem } = require('../helpers/fetchItem');
 const item = require('../mocks/item');
 
 describe('2 - Teste a função fetchItem', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se fetchItem é uma função', async () => {
+    expect(typeof (fetchItem)).toBe('function');
+  });
+  it('Verifica se a fetch foi chamada', () => {
+    fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Verifica se a mensagem "You must provide an url" foi retornada', async () => {
+    expect(await fetchItem()).toEqual(new Error('You must provide an url'));
+  });
+  it('Verifica se retorna o objeto computadorSearch', async () => {
+    expect(await fetchItem('MLB1615760527')).toEqual(item);
+  });
+  it('Testa se fetch utiliza a URL', async () => {
+    const url = 'https://api.mercadolibre.com/items/MLB1615760527';
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalledWith(url);
+  });
 });
